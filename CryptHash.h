b549b66e7466f7453639abf2fb588584140b8c8c
Alex Lin <alex.lin@skytraq.com.tw>
2014-08-22 15:47:58 +0800

    Revert "First commit"
    This reverts commit 32eef4fee54b0a3c49dfd2672496dbade8313a47.

--- CryptHash.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace CryptHash
-{
-	//std::wstring CryptHash::HashFileSHA1(const std::wstring & filename);
-	//std::wstring CryptHash::HashStringSHA1(const std::wstring & data);
-	CString HashStringSHA1(LPCSTR data);
-}
